989a8c716277675ee02d11460dc80c62bc2dee8b
Marco Prins <marco@kaartje2go.nl>
2021-03-12 11:25:20 +0100

    "Add ios methods"

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -4,5 +4,10 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(EspProvisioning, "EspProvisioning",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(createESPDevice, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(searchBleEspDevices, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(connectToDevice, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(scanWifiList, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(provision, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(scanQRCode, CAPPluginReturnPromise);
 )
